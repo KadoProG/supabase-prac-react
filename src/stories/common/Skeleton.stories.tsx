641be97ca790eae7da9fb5f69731aae9c847c333
KadoProG <splatrain@gmail.com>
2024-08-01 08:46:57 +0900

    add loading container

--- src/stories/common/Skeleton.stories.tsx
+++ src/stories/common/Skeleton.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Skeleton } from '@/components/common/Skeleton';
 
 const meta = {
-  title: 'common/surfaces/Skeleton',
+  title: 'common/Skeleton',
   component: Skeleton,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
